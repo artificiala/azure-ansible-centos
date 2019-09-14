801cfbef5adea7a44f2689d4e2586f68f832dbc3
Russ Mckendrick <russ@mckendrick.io>
2019-09-14 17:50:32 +0100

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-## Update and upgrade the packages
+## Update and install packages
 sudo yum -y update
+sudo yum -y install epel-release
+sudo yum -y install python2-pip
 
+## Install Ansible and Azure Modules using pip
+sudo pip install ansible[azure]
